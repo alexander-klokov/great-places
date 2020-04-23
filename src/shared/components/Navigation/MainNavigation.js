58019224ddebe477dde2c6d6f8bdf3f2ee8d65c6
Alexander Klokov <alexander.klokov@gmail.com>
2020-04-23 11:58:06 -0500

    add SideDrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,20 +3,29 @@ import {Link} from 'react-router-dom'
 
 import {MainHeader} from './MainHeader'
 import {NavLinks} from './NavLinks'
+import {SideDrawer} from './SideDrawer'
+
 import './MainNavigation.css'
 
 export const MainNavigation = props => (
-  <MainHeader>
-    <button className="main-navigation__menu-btn"> 
+    <React.Fragment>
+      {/* <SideDrawer>
+        <nav className="main-navigation__drawer-nav">N
+            <NavLinks />
+        </nav>
+      </SideDrawer> */}
+      <MainHeader>
+      <button className="main-navigation__menu-btn"> 
         <span />
         <span />
         <span />
-    </button>
-    <h1 className="main-navigation__title">
+      </button>
+      <h1 className="main-navigation__title">
         <Link to="/">GreatPlaces</Link>
-    </h1>
-    <nav>
+      </h1>
+      <nav className="main-navigation__header-nav">
         <NavLinks />
-    </nav>
-  </MainHeader>
+      </nav>
+    </MainHeader>
+  </React.Fragment>
 )
