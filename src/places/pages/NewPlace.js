53112221b9734c03aacbaa9ba76b74321cf76020
Alexander Klokov <alexander.klokov@gmail.com>
2020-04-29 21:23:53 -0500

    add NewPlace form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+import Input from '../../shared/components/FormElements/Input'
+import './NewPlace.css'
+
 export const NewPlace = () => {
-    return <h2>NewPlace works</h2>
+  return (
+    <form className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </form>
+  )
 }
