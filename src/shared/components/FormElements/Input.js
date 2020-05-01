a322c64ba15905c3d20412ee4fe2a140866099e9
Alexander Klokov <alexander.klokov@gmail.com>
2020-04-30 20:27:22 -0500

    complete AddPlace form

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react'
+import React, {useReducer, useEffect} from 'react'
 
 import {validate} from '../../util/validators'
 
@@ -44,6 +44,13 @@ const Input = props => {
       type: 'TOUCH' 
     })
   }
+
+  const {id, onInput} = props
+  const {value, isValid} = inputState
+
+  useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput])
 
   const element =
     props.element === 'input' ? (
